ea9ed9a44c26a302b7ad6695431d49b28b20d7b0
Anita Virag <ik.virag@gmail.com>
2019-04-08 21:47:06 +0200

    data, methods and data binding

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'Anita'
+    name: 'Anita',
+    job: 'ninja',
+    website: 'http://www.thenetninja.co.uk',
+    websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>'
+  },
+  methods: {
+    greet: function (time) {
+      return 'Good ' + time + ', ' + this.name;
+    }
   }
 });
