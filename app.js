8212eec9ce868ba7c244d7384457c8d9817bfc9a
Anita Virag <ik.virag@gmail.com>
2019-04-08 22:03:25 +0200

    events

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ new Vue({
     name: 'Anita',
     job: 'ninja',
     website: 'http://www.thenetninja.co.uk',
-    websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>'
+    websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>',
+    age: 25,
+    x: 0,
+    y: 0
   },
   methods: {
     greet: function (time) {
       return 'Good ' + time + ', ' + this.name;
+    },
+    add: function (inc) {
+      this.age += inc;
+    },
+    subtract: function (dec) {
+      this.age -= dec;
+    },
+    updateXY: function(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
