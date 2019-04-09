6e85953d86723eaff5f8eb596a6ffb51d87d44a3
Anita Virag <ik.virag@gmail.com>
2019-04-09 21:52:51 +0200

    dynamic css, conditionals, loop, template tag

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ new Vue({
     x: 0,
     y: 0,
     a: 0,
-    b: 0
+    b: 0,
+    available: false,
+    nearby: false,
+    error: false,
+    success: false,
+    characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+    ninjas: [
+        { name: 'Ryu', age: 25 },
+        { name: 'Yoshi', age: 35 },
+        { name: 'Ken', age: 55 }
+    ]
   },
   methods: {
     greet: function (time) {
@@ -43,6 +53,12 @@ new Vue({
     addToB: function() {
       console.log('addToB');
       return this.b + this.age;
+    },
+    compClasses: function() {
+      return {
+        available: this.available,
+        nearby: this.nearby
+      };
     }
   }
 });
